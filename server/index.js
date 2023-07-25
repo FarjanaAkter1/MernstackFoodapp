65ad5cc405f243780dc2ba5fcbc5e6a9cc4f36d9
Farjana Akter <fakter@xpanxion.com>
2023-07-24 21:46:36 -0400

    update frontend and backend

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const UserModel = require('./models/users');
-const cors = require("cors");
-app.use (express.json());
+const bcrypt = require('bcrypt');
+const cors = require('cors');
+const { UserModel, FoodModel, UserreviewModel } = require('./models/users');
+
+app.use(express.json());
 app.use(cors());
+
 const MONGO_URI = "mongodb+srv://fakter:Rahmanaariz1992$@cluster20.8gtbaai.mongodb.net/fooddata?retryWrites=true&w=majority";
 
 mongoose.connect(MONGO_URI, {
@@ -15,65 +18,98 @@ mongoose.connect(MONGO_URI, {
 })
   .then(() => {
     console.log('CONNECTED TO MongoDB');
-    
-    // Create a new user document and save it to the database
-    const newUser = new UserModel({
-      name: "Valentina",
-      age: 0,
-      username:"Valentina"
-
-
-    });
-
-
-
-    app.delete("/deleteUsers", async (req, res) => {
-      try {
-        // Delete all users from the collection
-        await UserModel.deleteMany({});
-        res.json({ message: "All users deleted successfully" });
-      } catch (error) {
-        res.json({ error: error.message });
-      }
-    });
-    
-
-
-
-
-
-
-    
-    newUser.save()
-      .then(savedUser => {
-        console.log("User saved:", savedUser);
-      })
-      .catch(error => {
-        console.log("Error saving user:", error);
-      });
   })
   .catch(error => {
-    console.log.error('ERROR: CONNECTING TO MongoDB: ', error);
+    console.error('ERROR: CONNECTING TO MongoDB: ', error);
+  });
+/////
+
+const newUserReview = new UserreviewModel({
+  name: "Noba ",
+  username: "Nobaab",
+  comment: "I absolutely loved the drink  and the service was Outstanding!"
+});
+
+newUserReview.save()
+  .then(savedUserReview => {
+    console.log("User review saved:", savedUserReview);
+  })
+  .catch(error => {
+    console.log("Error saving user review:", error);
   });
 
-app.get("/getUsers", async (req, res) => {
+  app.get("/userreviews", async (req, res) => {
+    try {
+      const userReviews = await UserreviewModel.find({});
+      res.json(userReviews);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+
+app.post('/register', async (req, res) => {
+  const { name, username, password } = req.body;
   try {
-    const users = await UserModel.find({});
-    res.json(users);
+    const existingUser = await UserModel.findOne({ username: username });
+    if (existingUser) {
+      return res.status(409).json({ message: 'Username already exists' });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new UserModel({
+      name: name,
+      username: username,
+      password: hashedPassword
+    });
+
+    await newUser.save();
+    res.json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await UserModel.findOne({ username: username });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    res.json({ message: 'Login successful' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+
+app.delete("/deleteUsers", async (req, res) => {
+  try {
+    await UserModel.deleteMany({});
+    res.json({ message: "All users deleted successfully" });
   } catch (error) {
     res.json({ error: error.message });
   }
 });
 
-app.post("/createUser",async(req, res) =>{
-  const user =req.body
-  const newUser = new UserModel(user)
-  await newUser.save();
+app.get("/foods", async (req, res) => {
+  try {
+    const foodItems = await FoodModel.find({});
+    res.json(foodItems);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-  res.json(user);
-
-})
-
-app.listen(3001, () => {
-  console.log('Server running on port 3001');
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
--- server/models/users.js
+++ server/models/users.js
@@ -1,20 +1,56 @@
+
 const mongoose = require('mongoose');
+
+const UserreviewSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  comment: {
+    type: String,
+    default: 'I had the most amazing dining experience at Delicious Delights!',
+  },
+});
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true
-  },
-  age: {
-    type: Number,
-    required: true
+    required: true,
   },
   username: {
     type: String,
-    required: true
-  }
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-const UserModel = mongoose.model('user', userSchema);
+const foodSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
 
-module.exports = UserModel;
+const UserModel = mongoose.model('User', userSchema);
+const FoodModel = mongoose.model('Food', foodSchema);
+const UserreviewModel = mongoose.model('UserReview', UserreviewSchema);
+
+module.exports = { UserModel, FoodModel, UserreviewModel };
